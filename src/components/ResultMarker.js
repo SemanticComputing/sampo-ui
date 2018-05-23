925eefa4b5320fece5ee059777cfdf0740dcbf68
esikkala <esko.ikkala@aalto.fi>
2018-05-23 21:29:52 +0300

    Ignore places with missing coordinates

--- src/components/ResultMarker.js
+++ src/components/ResultMarker.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import { Marker, Popup } from 'react-leaflet';
 
 const ResultMarker = ({ label, lat, long }) => {
-  const pos = [+lat[0].value, +long[0].value];
-  return (
-    <Marker position={pos}>
-      <Popup>
-        <p>{label[0].value}</p>
-      </Popup>
-    </Marker>
-  );
+  if (typeof lat === 'undefined' || typeof long === 'undefined') {
+    return(null);
+  } else {
+    const pos = [+lat[0].value, +long[0].value];
+    return (
+      <Marker position={pos}>
+        <Popup>
+          <p>{label[0].value}</p>
+        </Popup>
+      </Marker>
+    );
+  }  
 };
 
 ResultMarker.propTypes = {
