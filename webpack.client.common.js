fededa5297268eab4b276af8e0f02eb2db68741b
Esko Ikkala <esko.ikkala@aalto.fi>
2018-06-12 15:14:42 +0300

    Add build scripts

---
+++ webpack.client.common.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
+const outputDirectory = 'dist/public';
+
+module.exports = {
+  entry: {
+    app: './src/client/index.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Hipla',
+      // Load a custom template
+      template: 'src/client/index.html',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, outputDirectory),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-2']
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
---
+++ webpack.client.dev.js
@@ -0,0 +1,16 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.client.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 8080,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
+  }
+});
