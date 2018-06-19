ea0c956a87c43e6cac7651939dbcc649db9c5a69
Esko Ikkala <esko.ikkala@aalto.fi>
2018-06-19 12:10:54 +0300

    No static content with the dev server at the moment

--- webpack.client.dev.js
+++ webpack.client.dev.js
@@ -5,12 +5,8 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
     hot: true,
     port: 8080,
     open: true,
-    // proxy: {
-    //   '/api': 'http://localhost:3000'
-    // }
   }
 });
