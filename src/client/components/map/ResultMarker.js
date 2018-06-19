a738c675dd1d03f05b06c7e499ee55924aea97c7
Esko Ikkala <esko.ikkala@aalto.fi>
2018-06-19 12:52:34 +0300

    Testing custom marker icons

--- src/client/components/map/ResultMarker.js
+++ src/client/components/map/ResultMarker.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 
 const ResultMarker = ({ uri, label, typeLabel, broaderAreaLabel, source, lat, long }) => {
+  // const greenIcon = new L.Icon({
+  //   iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  //   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  //   iconSize: [25, 41],
+  //   iconAnchor: [12, 41],
+  //   popupAnchor: [1, -34],
+  //   shadowSize: [41, 41]
+  // });
+
   if (typeof lat === 'undefined' || typeof long === 'undefined') {
     return(null);
   } else {
     const pos = [+lat, +long];
     return (
-      <Marker position={pos}>
+      <Marker
+        position={pos}
+      >
         <Popup>
           <div>
             <h3>{label}</h3>
