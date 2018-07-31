754772ac6ff68380f2d139d860eaadd77beccb69
Esko Ikkala <esko.ikkala@aalto.fi>
2018-07-31 14:42:21 +0300

    Kotus: use ldf.fi endpoint

--- src/server/Datasets.js
+++ src/server/Datasets.js
@@ -227,8 +227,8 @@ module.exports = {
   'kotus': {
     'title': 'Institute for the Languages of Finland (Kotus) Digital Names archive',
     'shortTitle': 'DNA',
-    // 'endpoint': 'http://ldf.fi/kotus-digital-names-archive/sparql',
-    'endpoint': 'http://localhost:3037/ds/sparql',
+    'endpoint': 'http://ldf.fi/kotus-digital-names-archive/sparql',
+    // 'endpoint': 'http://localhost:3037/ds/sparql',
     // 'suggestionQuery': `
     //   PREFIX text: <http://jena.apache.org/text#>
     //   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
