60f2c15522dee8d4c1d23b9551c3406f5e44c867
Esko Ikkala <esko.ikkala@aalto.fi>
2018-09-17 11:42:02 +0300

    Update logos

--- src/client/containers/MapApp.js
+++ src/client/containers/MapApp.js
@@ -253,9 +253,8 @@ let MapApp = (props) => {
         <Paper className={classes.footer}>
           <img className={classes.aaltoLogo} src='img/logos/aalto-logo-white-no-background-small.png' alt='Aalto University logo'/>
           <img className={classes.uhLogo} src='img/logos/university-of-helsinki-logo-white-no-background-small.png' alt='University of Helsinki logo'/>
-          {/* <img className={classes.secoLogo} src='img/logos/seco-logo-white-no-background-small.png' alt='SeCo logo'/> */}
+          <img className={classes.secoLogo} src='img/logos/seco-logo-white-no-background-small.png' alt='SeCo logo'/>
           <img className={classes.heldigLogo} src='img/logos/heldig-logo-small.png' alt='HELDIG logo'/>
-          <img className={classes.kotusLogo} src='img/logos/kotus-logo-white-no-backgrounds-small.png' alt='Kotus logo'/>
         </Paper>
       </div>
     </div>
