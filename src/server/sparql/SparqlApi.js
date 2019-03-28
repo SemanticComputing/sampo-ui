65496e79d65c6d78aa0477250f33cf6f97dc56f7
esikkala <esko.ikkala@aalto.fi>
2019-03-28 16:24:15 +0200

    Replace node-fetch with axios

--- src/server/sparql/SparqlApi.js
+++ src/server/sparql/SparqlApi.js
@@ -1,5 +1,6 @@
-import fetch from 'node-fetch';
-const { URLSearchParams } = require('url');
+//import fetch from 'node-fetch';
+import axios from 'axios';
+// const { URLSearchParams } = require('url');
 
 const defaultSelectHeaders = {
   'Content-Type': 'application/x-www-form-urlencoded',
@@ -16,34 +17,48 @@ class SparqlApi {
     this.endpoint = endpoint;
   }
 
-  query(query, { headers }) {
-    return new Promise((resolve, reject) => {
-      const params = new URLSearchParams();
-      params.append('query', query);
-      fetch(this.endpoint, {
+  // query(query, { headers }) {
+  //   return new Promise((resolve, reject) => {
+  //     const params = new URLSearchParams();
+  //     params.append('query', query);
+  //     fetch(this.endpoint, {
+  //       method: 'post',
+  //       body:    params,
+  //       headers: headers,
+  //     })
+  //       .then(res => {
+  //         if (res.ok) { // res.status >= 200 && res.status < 300
+  //           return resolve(res.text());
+  //         } else {
+  //           return reject(res.statusText);
+  //         }
+  //       });
+  //   });
+  // }
+
+  selectQuery = async query => {
+    try {
+      const response = await axios({
         method: 'post',
-        body:    params,
-        headers: headers,
-      })
-        .then(res => {
-          if (res.ok) { // res.status >= 200 && res.status < 300
-            return resolve(res.text());
-          } else {
-            return reject(res.statusText);
-          }
-        });
-    });
+        headers: defaultSelectHeaders,
+        url: this.endpoint,
+        data: { query }
+      });
+      return JSON.parse(response);
+    } catch(error) {
+      console.error(error);
+    }
   }
 
-  selectQuery(query, params = { headers: defaultSelectHeaders }) {
-    return this.query(query, params).then(data => {
-      return JSON.parse(data);
-    });
-  }
-
-  constructQuery(query, params = { headers: defaultConstructHeaders }) {
-    return this.query(query, params);
-  }
+  // selectQuery(query, params = { headers: defaultSelectHeaders }) {
+  //   return this.query(query, params).then(data => {
+  //     return JSON.parse(data);
+  //   });
+  // }
+  //
+  // constructQuery(query, params = { headers: defaultConstructHeaders }) {
+  //   return this.query(query, params);
+  // }
 }
 
 export default SparqlApi;
