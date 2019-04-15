ffca5457c50a4b4360621c94e7d4b859a2b78a4b
esikkala <esko.ikkala@aalto.fi>
2019-04-15 08:12:34 +0300

    Add icons to material-table

--- src/client/components/facet_results/ResultTable2.js
+++ src/client/components/facet_results/ResultTable2.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MaterialTable from 'material-table';
+import SearchIcon from '@material-ui/icons/Search';
+import ClearIcon from '@material-ui/icons/Clear';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import LastPageIcon from '@material-ui/icons/LastPage';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 class ResultTable2 extends React.Component {
   render() {
@@ -15,6 +21,14 @@ class ResultTable2 extends React.Component {
           ]}
           data={[{ name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 }]}
           title="Demo Title"
+          icons={{
+            Search: SearchIcon,
+            ResetSearch: ClearIcon,
+            FirstPage: FirstPageIcon,
+            LastPage: LastPageIcon,
+            NextPage: ChevronRightIcon,
+            PreviousPage: ChevronLeftIcon
+          }}
         />
       </div>
     );
