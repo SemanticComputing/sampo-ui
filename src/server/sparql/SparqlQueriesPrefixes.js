fb30abe6cd10114902eafe4e8af0ad98ffb0e56e
esikkala <esko.ikkala@aalto.fi>
2019-04-16 11:04:56 +0300

    Switch to use Erlangen CRM

--- src/server/sparql/SparqlQueriesPrefixes.js
+++ src/server/sparql/SparqlQueriesPrefixes.js
@@ -2,7 +2,7 @@ export const prefixes = `
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-  PREFIX crm: <http://www.cidoc-crm.org/cidoc-crm/>
+  PREFIX crm: <http://erlangen-crm.org/current/>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
   PREFIX text: <http://jena.apache.org/text#>
