8558cba8ae6d2627c3b331fbf23b59575348c173
esikkala <esko.ikkala@aalto.fi>
2020-02-15 12:34:40 +0200

    Upgrade leaflet

--- src/client/configs/mmm/GeneralConfig.js
+++ src/client/configs/mmm/GeneralConfig.js
@@ -6,7 +6,7 @@ export const showLanguageButton = false
 
 export const publishedPort = 3006
 
-export const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoiZWtrb25lbiIsImEiOiJjam5vampzZ28xd2dyM3BzNXR0Zzg4azl4In0.eozyF-bBaZbA3ibhvJlJpQ'
+export const MAPBOX_ACCESS_TOKEN = ''
 
 export const documentFinderAPIUrl = 'https://data.finlex.fi/document-finder-backend'
 
