62b088f5b37385de50231d1d474e52f9f7397a00
esikkala <esko.ikkala@aalto.fi>
2020-05-05 15:27:57 +0300

    Configure the Redux store in a separate file

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware, compose, bindActionCreators } from 'redux'
-import { createEpicMiddleware } from 'redux-observable'
 import { Provider } from 'react-redux'
-import ReduxToastr, { actions as toastrActions } from 'react-redux-toastr'
+import ReduxToastr from 'react-redux-toastr'
 import { Router } from 'react-router-dom'
 import history from './History'
-import reducer from './reducers'
-import rootEpic from './epics'
+import configureStore from './configureStore'
 import App from './components/App'
 import { availableLocales } from './epics/index.js'
 import { loadLocales } from './actions'
@@ -19,22 +16,7 @@ import 'react-sortable-tree/style.css'
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
 
-const epicMiddleware = createEpicMiddleware()
-
-const middleware = applyMiddleware(epicMiddleware)
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(
-  reducer,
-  compose(
-    composeEnhancers(middleware)
-  )
-)
-
-epicMiddleware.run(rootEpic)
-
-bindActionCreators(toastrActions, store.dispatch)
+const store = configureStore()
 
 // init locale
 const localeFromUrl = window.location.pathname.substr(1, 2)
