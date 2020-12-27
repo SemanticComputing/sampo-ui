dd0db389675b49c7e004b366176fb0e95e269aec
esikkala <esko.ikkala@aalto.fi>
2020-12-27 15:19:27 +0200

    Add output folder for images

--- webpack.client.common.js
+++ webpack.client.common.js
@@ -56,9 +56,10 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
+        loader: 'file-loader',
+        options: {
+          outputPath: 'images'
+        }
       }
     ]
   },
