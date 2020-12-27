edd6f8c336a693d1411b507563cd89a318daade6
esikkala <esko.ikkala@aalto.fi>
2020-12-27 18:28:12 +0200

    Separate Babel configs for browser and Node.js

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,33 @@
 module.exports = function (api) {
   api.cache(true)
-
-  const presets = [
-    '@babel/preset-env',
-    '@babel/preset-react'
-  ]
-  const plugins = [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime' // for async/await in node
-  ]
+  const isBrowser = process.env.BABEL_ENV === 'browser'
+  const presets = []
+  const plugins = []
+  if (isBrowser) {
+    presets.push([
+      '@babel/preset-env',
+      {
+        /**
+         * Target Browserslist’s default browsers.
+         * https://github.com/browserslist/browserslist
+         */
+        targets: 'defaults'
+      }
+    ])
+    presets.push('@babel/preset-react')
+    plugins.push('@babel/plugin-proposal-class-properties')
+    plugins.push('@babel/plugin-transform-runtime')
+  }
+  if (!isBrowser) {
+    presets.push([
+      '@babel/preset-env',
+      {
+        targets: {
+          node: '14'
+        }
+      }
+    ])
+  }
 
   return {
     presets,
