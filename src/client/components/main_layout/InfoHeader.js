7f78bcfe23b01ec625c030b4842243aa8e7ab2e5
esikkala <esko.ikkala@aalto.fi>
2021-04-21 13:45:14 +0300

    Info header: handle missing prefLabel

--- src/client/components/main_layout/InfoHeader.js
+++ src/client/components/main_layout/InfoHeader.js
@@ -75,7 +75,7 @@ const InfoHeader = props => {
     let label = ''
     const data = props.instanceData
     const hasData = data !== null && Object.values(data).length >= 1
-    if (hasData) { label = data.prefLabel.prefLabel || data.prefLabel }
+    if (hasData && data.prefLabel) { label = data.prefLabel.prefLabel || data.prefLabel }
     return label
   }
 
